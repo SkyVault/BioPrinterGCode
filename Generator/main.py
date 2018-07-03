f70d3bf7f4acad6a21e5895b4c5b0374bc80bcb2
Dustin Neumann <dustin.neumann@wallawalla.edu>
2018-07-03 08:49:55 -0700

    We now save fields

--- Generator/main.py
+++ Generator/main.py
@@ -1,9 +1,99 @@
 #!/usr/bin/env python
 
+import pickle
 import Tkinter as tk
+from Tkinter import LEFT, Grid, Frame
 
 from functools import *
 from ntpath import basename
+import os.path
+
+FONT = 32
+SAVE = "save"
+
+ToSave = {}
+
+if os.path.isfile(SAVE):
+    with open(SAVE, 'rb') as f:
+        ToSave = pickle.load(f)
+else:
+    ToSave = {
+        "outfile": "zamboni_1.ngc",
+        "template": "zamboni.ngc_template"
+    }
+    with open(SAVE, 'wb') as f:
+        pickle.dump(ToSave, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+print(ToSave)
+
+def Save():
+    with open(SAVE, 'wb') as f:
+        pickle.dump(ToSave, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+class FrameGroup(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.all_instances = []
+        self.counter = 0
+
+    def Add(self):
+        self.counter += 1
+        name = "Frame %s" % self.counter 
+        subframe = Subframe(self, name=name)
+        subframe.pack(side="left", fill="y")
+        self.all_instances.append(subframe)
+
+    def Remove(self, instance):
+        # don't allow the user to destroy the last item
+        if len(self.all_instances) > 1:
+            index = self.all_instances.index(instance)
+            subframe = self.all_instances.pop(index)
+            subframe.destroy()
+
+    def HowMany(self):
+        return len(self.all_instances)
+
+    def ShowMe(self):
+        for instance in self.all_instances:
+            print(instance.get())
+
+
+class Subframe(tk.Frame):
+    def __init__(self, parent, name):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.e1 = tk.Entry(self)
+        self.e2 = tk.Entry(self)
+        self.e3 = tk.Entry(self)
+        label = tk.Label(self, text=name, anchor="center")
+        add_button = tk.Button(self, text="Add", command=self.parent.Add)
+        remove_button = tk.Button(self, text="Remove", command=lambda: self.parent.Remove(self))
+
+        label.pack(side="top", fill="x")
+        self.e1.pack(side="top", fill="x")
+        self.e2.pack(side="top", fill="x")
+        self.e3.pack(side="top", fill="x")
+        add_button.pack(side="top")
+        remove_button.pack(side="top")
+
+
+def makeFilepathInput(parent, default_path="C:/"):
+    frame = Frame(parent)
+
+    outvar = tk.StringVar()
+    outvar.set(default_path)
+    entry = tk.Entry(frame, textvariable=outvar)
+    entry.config(font=("Courier", FONT))
+    entry.pack(side="left")
+
+    def test():
+        print("Hello")
+
+    open_dialog = tk.Button(frame, text="...", command=test)
+    open_dialog.config(font=("Courier", FONT / 2))
+    open_dialog.pack(side="left")
+
+    return (frame, outvar)
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -11,22 +101,21 @@ class Application(tk.Frame):
         self.grid()
 
     def createWidgets(self, varmap, content):
-        FONT = 32
         self.content = content
         self.varmap = varmap
         
         # Create the export filepath entry
-        self.filepathoutvar = tk.StringVar()
-        self.filepathoutvar.set("zambonie_1.ngc")
-        self.filepathoutentry = tk.Entry(self, textvariable=self.filepathoutvar)
-        self.filepathoutentry.grid(row=0, column=1)
-        self.filepathoutentry.config(font=("Courier", FONT))
+        # self.filepathoutvar = tk.StringVar()
+        # self.filepathoutvar.set("zambonie_1.ngc")
+        # self.filepathoutentry = tk.Entry(self, textvariable=self.filepathoutvar)
+        # self.filepathoutentry.grid(row=0, column=1)
+        # self.filepathoutentry.config(font=("Courier", FONT))
 
-        self.filepathvar = tk.StringVar()
-        self.filepathvar.set("zambonie.ngc_template")
-        self.filepathentry = tk.Entry(self, textvariable=self.filepathvar)
+        self.filepathoutentry, self.filepathoutvar = makeFilepathInput(self, default_path=ToSave["outfile"])
+        self.filepathoutentry.grid(row=0, column=1)
+
+        self.filepathentry, self.filepathvar = makeFilepathInput(self, default_path=ToSave["template"])
         self.filepathentry.grid(row=0, column=0)
-        self.filepathentry.config(font=("Courier", FONT))
 
         self.stringvars = {}
 
@@ -50,21 +139,26 @@ class Application(tk.Frame):
             index += 1
 
         index += 1
+
         self.generateButton = tk.Button(self, text="Generate", command=self.generatePart)
         self.generateButton.grid(row=index, column=0) 
         self.generateButton.config(font=("Courier", FONT))
 
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.grid(row=index, column=1)
-        self.quitButton.config(font=("Courier", FONT))
+        self.btn_frame = Frame(self)
+        self.btn_frame.grid(column=1, row=index)
 
-        self.decButton = tk.Button(self, text="-", command= lambda: self.addFileName(-1))
-        self.decButton.grid(row=index, column=2)
+        self.decButton = tk.Button(self.btn_frame, text="-", command= lambda: self.addFileName(-1))
         self.decButton.config(font=("Courier", FONT))
+        self.decButton.pack(side="left")
 
-        self.incButton = tk.Button(self, text="+", command= lambda: self.addFileName(1))
-        self.incButton.grid(row=index, column=3)
+        self.incButton = tk.Button(self.btn_frame, text="+", command= lambda: self.addFileName(1))
         self.incButton.config(font=("Courier", FONT))
+        self.incButton.pack(side="left")
+
+        self.quitButton = tk.Button(self.btn_frame, text='Quit', command=self.quit)
+        self.quitButton.config(font=("Courier", FONT))
+        self.quitButton.pack(side="left")
+
 
     def addFileName(self, val):
         path = self.filepathoutvar.get()
@@ -75,6 +169,8 @@ class Application(tk.Frame):
         num = int(num)
         num += val
         self.filepathoutvar.set("{}_{}.{}".format(name, num, ext))
+        ToSave["outfile"] = self.filepathoutvar.get()
+        Save()
 
     def generatePart(self):
         values = []
@@ -92,7 +188,7 @@ def run():
     
     varmap = {}
 
-    with open("zamboni.ngc_template", "r") as f:
+    with open(ToSave["template"], "r") as f:
         lines = f.readlines() 
         f.seek(0)
         content = f.read()
